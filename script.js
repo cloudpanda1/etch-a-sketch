0be54086e116eb303fa56a599ba13a487cf9c195
cloud <73915583+cloudpanda1@users.noreply.github.com>
2022-02-12 21:05:47 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const row1 = document.getElementById("row1");
+const row2 = document.getElementById("row2");
+const row3 = document.getElementById("row3");
+const row4 = document.getElementById("row4");
+const row5 = document.getElementById("row5");
+const row6 = document.getElementById("row6");
+
+for (let i = 0; i < 7; i++) {
+  row1.innerHTML += "<div id='item1'></div>";
+  row2.innerHTML += "<div id='item1'></div>";
+  row3.innerHTML += "<div id='item1'></div>";
+  row4.innerHTML += "<div id='item1'></div>";
+  row5.innerHTML += "<div id='item1'></div>";
+  row6.innerHTML += "<div id='item1'></div>";
+}
